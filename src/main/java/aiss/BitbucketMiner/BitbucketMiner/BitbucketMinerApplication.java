e587f66138762767a55549a08e44259bae1c1b9e
Enrique Cichy <enrpurcic@alum.us.es>
2025-05-01 17:31:25 +0200

    Probando commit

--- src/main/java/aiss/BitbucketMiner/BitbucketMiner/BitbucketMinerApplication.java
+++ src/main/java/aiss/BitbucketMiner/BitbucketMiner/BitbucketMinerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BitbucketMinerApplication {
 
+	//PROBANDO COMMIT
+
 	public static void main(String[] args) {
 		SpringApplication.run(BitbucketMinerApplication.class, args);
 	}
